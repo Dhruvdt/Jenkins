052c41e7991723117b21c61547948feec6833400
Dhruv <dhruvtyagi88@gmail.com>
2025-04-07 16:26:48 +0530

    Add TestController with hello endpoint

---
+++ src/main/java/com/docker/jenkins/controller/TestController.java
@@ -0,0 +1,16 @@
+package com.docker.jenkins.controller;
+
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.*;
+
+@Validated
+@RestController
+@RequestMapping(value = "/api/v1/admin/")
+@CrossOrigin(origins = "*", allowedHeaders = "*")
+public class TestController {
+    @GetMapping("/hello")
+    public String sayHello() {
+      return "Hello from Spring Boot!";
+    }
+
+}
